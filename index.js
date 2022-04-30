7a796e6b851195e60223027deeb89865695d27b2
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-04-30 23:03:44 +0600

    pagination problem solve

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
@@ -44,6 +44,17 @@ async function run() {
     app.get("/productCount", async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count });
+    });
+
+    //use post to get products bt keys
+    app.post("/productByKeys", async (req, res) => {
+      const keys = req.body;
+      const ids = keys.map((id) => ObjectId(id));
+      const query = { _id: { $in: ids } };
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+      console.log(keys);
     });
   } finally {
   }
